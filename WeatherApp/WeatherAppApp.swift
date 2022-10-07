4170b704082c43da05ec7f7f0c131f6c3d4f10b4
HIDROXIDO <soutoinf@gmail.com>
2022-10-07 09:00:54 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Alexandre Souto on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
